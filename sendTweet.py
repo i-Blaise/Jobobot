d6f951c86a1443952a7ce0560a4533e9fa18580e
Your Name <menniablaise@yahoo.com>
2024-09-25 09:36:32 +0000

    sendTweet working

--- sendTweet.py
+++ sendTweet.py
@@ -1,10 +1,10 @@
 import tweepy
 
-access_token = '1793711715835666432-xNWv8Aq7iKV6xgLN2xyZdl18hf5nLK'
-access_token_secret = '4sXlZzeN5kxAiXDOTqPEHaafsXPHFXONUjwLosQ6ZZ3oY'
-api_key = 'tYfNUd6kU2RcZj8m60lCG731D'
-api_key_secret = 'pTk92Ze6XyzkjohUGtYa915hS2QE6qfM9aCKfRn96RlJGLPHRe'
-bearer_token = "AAAAAAAAAAAAAAAAAAAAACUXwAEAAAAAA4afc9YZfh8jBYvFJ%2Fu3yuDFZdo%3DHUtWlFfpA0ZcQCqjEvaoVXy2GJccC5HDqQIexbYgEgNHc7B009" 
+access_token = "1793711715835666432-az5RXZ6j1h8AnaKFERGablXGZjYIws"
+access_token_secret = "RLWysRilQQDSer3HndFE16AFEibM6LvAlu7vaBu0AzISb"
+bearer_token = r"AAAAAAAAAAAAAAAAAAAAACUXwAEAAAAAUJENIAH%2BbGK%2BqWjQ3V5PiVT2OEI%3DaDxhIX3mNZdjlejcnkZG7PiGdKPNe2Qc8Ba3NNboxHqbovSJTP"
+api_key = "FUNlslh7IpEji2ZEnu95lky4q" #api-key
+api_secret = "DaCcAnkCcLt0s1MMFAfI5VTZHKnFyuyz9H1r3Aq6Dmk4BRjuD6" #api key secret
 client_id = 'NUh4U1k4ZDd5SFVjVXFZU3Rjb0s6MTpjaQ'
 client_secret = 'hFSS8dSCesfv2xG4FAoKch6HRkjABeuhN-SUPkn0z3avt0ZZK_'
 
@@ -12,20 +12,40 @@ client_secret = 'hFSS8dSCesfv2xG4FAoKch6HRkjABeuhN-SUPkn0z3avt0ZZK_'
 # auth.set_access_token(access_taken, access_token_secret)
 # api = tweepy.API(auth, wait_on_rate_limit=True)
 
+# Set up OAuth and integrate with Tweepy
+# auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret, access_token, access_token_secret)
+# api = tweepy.API(auth)
+
 client = tweepy.Client(
-    bearer_token = bearer_token,
-    consumer_key = client_id, 
-    consumer_secret = client_secret, 
-    access_token = access_token, 
-    access_token_secret = access_token_secret,
-    wait_on_rate_limit=True
+    bearer_token,
+    api_key, 
+    api_secret, 
+    access_token, 
+    access_token_secret
 )
 
-# response = client.create_tweet(
-#     text='hello world!'
-# )
 
-text = 'Hello World'
+auth = tweepy.OAuth1UserHandler(api_key, api_secret, access_token, access_token_secret)
+api = tweepy.API(auth)
 
-client.create_tweet(text=text)
-print('tweeted')
+client.create_tweet(
+    text="Hello World!"
+)
+
+# Post a tweet
+# tweet = "Hello, world! :)"
+# api.update_status(status=tweet)
+
+# try:
+#     api.update_status(status=tweet)
+#     print("Tweet posted successfully!")
+# except tweepy.TweepyException as e:
+#     print(f"Error occurred: {e}")
+
+
+# print("Tweet posted successfully!")
+
+# text = 'Hello World'
+
+# client.create_tweet(text=text)
+# print('tweeted')
